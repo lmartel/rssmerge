94d64c575ee3eb8cee6be976709476e30fb4d91d
Leo Martel <leo@lpm.io>
2018-09-15 20:49:36 -0700

    switch out feed parser for a more permissive one

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 ruby '~> 2.4.4'
 
 gem 'algorithms'
+gem 'feedjira'
 gem 'parallel'
 gem 'sinatra'
 gem 'thin'
--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'open-uri'
+require 'feedjira'
 require 'parallel'
 require 'rss'
 require 'sinatra'
@@ -8,22 +8,46 @@ require './lib'
 # allow // in paths
 set :protection, except: :path_traversal
 
+# unofficial name: official name
+ATTR_SYNONYMS = {
+    url: [:link]
+}
+
+def copy_attrs_safely(from, to)
+  getters = from.public_methods(false).reject {|met| met.to_s.end_with?("=") }
+  getters.each do |getter|
+    names_to_try = [getter] + (ATTR_SYNONYMS[getter] || [])
+    names_to_try.each do |name|
+      setter = "#{name}=".to_sym
+      if to.respond_to?(setter)
+        value = from.send(getter)
+        to.send(setter, value)
+      end
+    end
+  end
+end
+
 get '/x/*' do
   # use lookahead regex to split /x/url1/url2 into [url1, url2], dropping the x
   _, *urls = request.fullpath.split(%r{/(?=https?://)})
   return 404 unless urls.length > 0
   feeds = Parallel.map(urls, in_threads: urls.length) do |url|
-    open(url) do |rss|
-      RSS::Parser.parse(rss)
-    end
+    Feedjira::Feed.fetch_and_parse(url)
   end
 
   # sort items from all feeds by date descending
-  merged_items = merge_sorted_arrays(feeds.map(&:items)) { |x, y| (x.date <=> y.date) == 1 }
+  merged_items = merge_sorted_arrays(feeds.map(&:entries)) { |x, y| (x.published <=> y.published) == 1 }
 
-  # we can't directly set feed.items so we modify the existing array instead
-  main_feed = feeds.first
-  main_feed.items.reject! { |_| true }
-  main_feed.items.push(*merged_items)
-  main_feed.to_s
+  primary = feeds.first
+  merged = RSS::Maker.make("rss2.0") do |maker|
+    copy_attrs_safely(primary, maker)
+    copy_attrs_safely(primary, maker.channel)
+
+    merged_items.each do |item|
+      maker.items.new_item do |new_item|
+        copy_attrs_safely(item, new_item)
+      end
+    end
+  end
+  merged.to_s
 end
